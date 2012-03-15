aeb3763629c2d157e67b0383a6aaef32e6a329ef
Richard Venable <richard@epicfox.com>
2012-03-14 20:19:33 -0500

    Added support for root plugin options

--- bfox_root_plugin_controller.php
+++ bfox_root_plugin_controller.php
@@ -32,6 +32,27 @@ class BfoxRootPluginController extends BfoxBaseRootPluginController {
 		$path = $this->templatePath($template);
 		load_template($path);
 	}
+
+	private $_options;
+	function options() {
+		if (is_null($this->_options)) $this->_options = get_option($this->prefix . '_options', array());
+		return $this->_options;
+	}
+
+	function saveOptions() {
+		update_option($this->prefix . '_options', $this->options());
+	}
+
+	function option($name) {
+		$options = $this->options();
+		if (isset($options[$name])) return $options[$name];
+		return null;
+	}
+
+	function setOption($name, $value) {
+		$this->_options[$name] = $value;
+		$this->saveOptions();
+	}
 }
 
 ?>
